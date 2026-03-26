92cfe261135abd81d306a34de88d9edf12010a21
StormmInABottle <sarahjohnyay@gmail.com>
2026-03-26 12:09:28 -0500

    finished image filtering

--- asd-projects/image-filtering/index.js
+++ asd-projects/image-filtering/index.js
@@ -20,7 +20,16 @@ function resetAndRender() {
 // all of your apply functions
 function applyAndRender() {
   // Multiple TODOs: Call your apply function(s) here
+  // applyFilterNoBackground(decreaseBlue);
+  // applyFilter(increaseGreenByBlue);
+  // applyFilterNoBackground(reddify);
+  // applyFilter(increaseGreenByBlue)
 
+  applyFilter(grayscale)
+  applyFilterNoBackground(increaseGreenByBlue)
+  applyFilterNoBackground(decreaseBlue)
+  applyFilter(reddify)
+  applyFilter(grayscale)
   
 
   // do not change the below line of code
@@ -33,17 +42,78 @@ function applyAndRender() {
 
 // TODO 1, 2, 3 & 5: Create the applyFilter function here
 
+function applyFilter(filterFunction) {
+  for (let i = 0; i < image.length; i++) {
+    row = image[i]
+    for (let j = 0; j < row.length; j++) {
+      var pixel = image[i][j]
+      pixelArray = rgbStringToArray(pixel)
+      // This is where I’ll modify the color values later
+      filterFunction(pixelArray)
+    
+      updatedPixel = rgbArrayToString(pixelArray)
+      image[i][j] = updatedPixel
+    }
+  }
+}    
 
 // TODO 9 Create the applyFilterNoBackground function
+
+function applyFilterNoBackground(filterFunction){
+  var backgroundColor = image[0][0];
+  for (let i = 0; i < image.length; i++) {
+      row = image[i]
+      for (let j = 0; j < row.length; j++) {
+        var pixel = image[i][j]
+        if (pixel !== backgroundColor){
+          var pixelArray = rgbStringToArray(pixel);
+          filterFunction(pixelArray);
+          var updatedString = rgbArrayToString(pixelArray);
+          image[i][j] = updatedString;
+        }
+      }
+  }
+}
 
 
 // TODO 6: Create the keepInBounds function
 
+function keepInBounds(number) {
+  if (number < 0) {
+    return 0
+  }
+  else if (number > 255) {
+    return 255
+  }
+  else {
+    return number
+  }
+}
 
 // TODO 4: Create reddify filter function
-
+function reddify(pixelArray) {
+  pixelArray[RED] = 200
+}
 
 // TODO 7 & 8: Create more filter functions
+function decreaseBlue(pixelArray){
+  pixelArray[BLUE] -= 50
+  keepInBounds(pixelArray[BLUE])
+}
 
+function increaseGreenByBlue(pixelArray){
+  pixelArray[GREEN] += pixelArray[BLUE];
+  keepInBounds(pixelArray[GREEN])
+
+}
 
 // CHALLENGE code goes below here
+
+function grayscale(pixelArray){
+  var averageValue = (pixelArray[GREEN] + pixelArray[BLUE] + pixelArray[RED]) / 3
+  console.log(pixelArray)
+  console.log("avgV: " + averageValue)
+  pixelArray[GREEN] = averageValue
+  pixelArray[BLUE] = averageValue
+  pixelArray[RED] = averageValue
+}
